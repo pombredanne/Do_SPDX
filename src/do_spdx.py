f3d858e24de94e0df3155928d28bc84ea1f5def1
Corbin Haughawout <corbin.haughawout@gmail.com>
2014-03-23 15:49:27 -0500

    Fixed indentation

--- src/do_spdx.py
+++ src/do_spdx.py
@@ -118,11 +118,11 @@ class DoSpdx():
 
 	def _get_filepaths(directory):
 		"""
-	    This function will generate the file names in a directory
-	    tree by walking the tree either top-down or bottom-up. For each
-	    directory in the tree rooted at directory top (including top itself),
-	    it yields a 3-tuple (dirpath, dirnames, filenames).
-	    """
+		This function will generate the file names in a directory
+		tree by walking the tree either top-down or bottom-up. For each
+		directory in the tree rooted at directory top (including top itself),
+		it yields a 3-tuple (dirpath, dirnames, filenames).
+		"""
 	    file_paths = []  # List which will store all of the full filepaths.
 
 	    # Walk the tree.
